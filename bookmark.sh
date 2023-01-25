93ac0a83414a6f3c0bcef15e1252746896dd6afe
devgioele <gioele@devgioele.xyz>
2023-01-25 17:23:29 +0100

    Add git and edit commands

--- bookmark.sh
+++ bookmark.sh
@@ -13,15 +13,21 @@ alias menu="tofi --width 90% --height 90% --font-size 16 --prompt-text"
 # and to be used as input field.
 # Despite no match, the input is printed to stdout.
 alias menu-input="tofi --require-match false --font-size 16 --padding-top 25 --padding-bottom 25 --padding-left 35 --padding-right 25 --prompt-text"
-file="${XDG_DATA_HOME:-$HOME/.local/share}/bookmarking/bookmarks"
+store_dir="${XDG_DATA_HOME:-$HOME/.local/share}/bookmarking"
+store_file="$store_dir/bookmarks"
 ##########
 
-# Create necessary dirs
-mkdir -p "$(dirname $file)"
+usage() {
+  echo "Usage: $PROGRAM <add|remove|pick|git|edit>"
+}
 
-alias help="echo \"Choose 'add', 'remove' or 'pick'.\""
-alias bookmark_to_pick="cat $file | menu 'Pick: '"
-alias bookmark_to_remove="cat $file | menu 'Remove: '"
+bookmark_to_pick() {
+  cat $store_file | menu "Pick: "
+}
+
+bookmark_to_remove() {
+  cat $store_file | menu "Remove: "
+}
 
 # Asks the user for tags recursively,
 # listing what tags exist so far.
@@ -41,16 +47,32 @@ ask_tag() {
   fi
 }
 
-if [ "$#" -ne 1 ]
+
+
+PROGRAM="${0##*/}"
+COMMAND="$1"
+
+# Create necessary dirs
+mkdir -p "$(dirname $store_file)"
+
+if [ "$#" -lt 1 ]
 then
-  echo "Invalid number of arguments."
-  help
+  usage
   exit 1
 fi
 
-if [ "$1" = "add" ]
+if [ "$COMMAND" = "git" ]
 then
-  # Add the current selection to the file
+  # Forward arguments as if git was called from the store directory
+  shift
+  git -C "$store_dir" "$@"
+elif [ "$COMMAND" = "edit" ]
+then
+  # Open the preferred editor to edit the store file
+  "$EDITOR" "$store_file"
+elif [ "$COMMAND" = "add" ]
+then
+  # Add the current selection to the store_file
   bookmark="$(wl-paste --primary 2> /dev/null)"
   if [ -n "$bookmark" ]
   then
@@ -61,12 +83,12 @@ then
       exit 1
     fi
     # Verify existence of bookmark, ignoring any tags
-    if grep -qP "^$bookmark(?= )" "$file"
+    if grep -qP "^$bookmark(?= )" "$store_file"
     then
       notify-send "Already bookmarked!"
     else
       # Prompt for tags, listing existing tags
-      existing_tags="$(cat "$file" | grep -Po "(?<= #)[^ ]+" | sort -u)"
+      existing_tags="$(cat "$store_file" | grep -Po "(?<= #)[^ ]+" | sort -u)"
       # `sort` does not append a newline to the last line when printing to stdout.
       # Append the newline if the string is not empty.
       if [ -n "$existing_tags" ]
@@ -77,32 +99,32 @@ then
       n=1
       ask_tag
       bookmark_with_tags="$bookmark$(printf "$tags" | tr '\n' ' ')"
-      echo "$bookmark_with_tags" >> "$file"
+      echo "$bookmark_with_tags" >> "$store_file"
       notify-send "Bookmark added!" "$bookmark"
     fi
   else
     notify-send "Clipboard is empty!" "Nothing to bookmark"
   fi
-elif [ "$1" = "remove" ]
+elif [ "$COMMAND" = "remove" ]
 then
-  # Remove the chosen bookmark from the file
+  # Remove the chosen bookmark from the store_file
   bookmark_with_tags="$(bookmark_to_remove)"
   if [ -n "$bookmark_with_tags" ]
   then
-    if sed -i "\|^$bookmark_with_tags$|d" "$file"
+    if sed -i "\|^$bookmark_with_tags$|d" "$store_file"
     then
       notify-send "Bookmark removed!" "$bookmark_with_tags"
     else
       notify-send "Bookmark does not exist!" "$bookmark_with_tags"
     fi
   fi
-elif [ "$1" = "pick" ]
+elif [ "$COMMAND" = "pick" ]
 then
   # Let the user choose a bookmark and copy the chosen bookmark
   bookmark_with_tags="$(bookmark_to_pick)"
   if [ -n "$bookmark_with_tags" ]
   then
-    if grep -q "^$bookmark_with_tags$" "$file"
+    if grep -q "^$bookmark_with_tags$" "$store_file"
     then
       # Copy the bookmark without the tags
       printf "$bookmark_with_tags" | grep -Eo "^[^ ]+" | tr -d '\n' | wl-copy
@@ -112,6 +134,6 @@ then
     fi
   fi
 else
-  echo "Unknown option!"
-  help
+  echo "Unknown command!"
+  usage
 fi
